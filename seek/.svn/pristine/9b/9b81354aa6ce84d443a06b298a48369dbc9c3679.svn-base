26d226c132f7ee00e57761559587a49784539a61
qiupengfei <qiupengfei921120@163.com>
2013-12-07 09:50:25 +0800

    v0.1
    qiu

--- seek/.svn/pristine/9b/9b81354aa6ce84d443a06b298a48369dbc9c3679.svn-base
+++
@@ -1,6 +0,0 @@
-﻿您的证据已提交成功
-<html>
-<body>
-<h1><?=anchor('', 'Congratulations!')?><h1>
-</body>
-</html>
--- seek/1/application/views/congratulation.php
+++
@@ -1,6 +0,0 @@
-﻿您的问题已提交成功，等待别人解答
-<html>
-<body>
-<h1><?=anchor('', 'Congratulations!')?><h1>
-</body>
-</html>
